585fe629e82a2421504c366fae9e1e4ba9265211
Victor <victor.agnez@hotmail.com>
2018-10-02 18:21:55 -0300

    Handle Client Exceptions

--- projeto1-RMI/src/client/Client.java
+++ projeto1-RMI/src/client/Client.java
@@ -9,21 +9,46 @@ import java.util.Scanner;
 import javafx.util.Pair;
 
 public class Client {
-	public static void main(String[] args) throws RemoteException, 
-		NotBoundException, MalformedURLException {
+	public static void main(String[] args) {
 
-		System.out.println("Digite o tamanho do vetor.");
 		
+		boolean invalid;
 		Scanner entrada = new Scanner(System.in);
-		Integer n = Integer.valueOf(entrada.next());
+		Integer n = 0;
+		
+		do {
+			System.out.println("Digite o tamanho do vetor.");
+			invalid = false;
+			try {
+				n = Integer.valueOf(entrada.next());
+				if(n <= 0) {
+					System.out.println("Valor inválido - digite um valor inteiro positivo.");
+					invalid = true;
+				}
+			} catch(NumberFormatException e) {
+				System.out.println("Valor inválido - digite um valor inteiro positivo.");
+				invalid = true;
+			}
+		} while(invalid);
 		
 		Integer[] vet = new Integer[n];
 		
 		System.out.println("Digite o vetor.");
 		
-		//Leitura do vetor
-		for(int i = 0; i < n; i++)
-			vet[i] = Integer.valueOf(entrada.next());
+		do {
+			invalid = false;
+			int i = 0;
+			try {
+				//Leitura do vetor
+				for(i = 0; i < n; i++)
+					vet[i] = Integer.valueOf(entrada.next());
+			} catch(NumberFormatException e) {
+				invalid = true;
+				System.out.println("Erro ao ler o elemento de índice " + i);
+				System.out.println("O vetor deve ser composto apenas por valores inteiros de 32 bits");
+				System.out.println("Digite o vetor novamente.");
+			}
+		} while(invalid);
 		
 		Compute.SortType type = null;
 		
@@ -57,20 +82,81 @@ public class Client {
 		Compute.Order order = (ord.equals("0") ? Compute.Order.Increasing : Compute.Order.Decreasing );
 		
 		//Recebe o vetor ordenado e o tempo em nano-segundos
-		Pair<Integer[], Long> result = Comunicate(type, order, vet);
+		Pair<Integer[], Long> result = null;
+		boolean repeat;
 		
-		vet = result.getKey();
+		int tryCount = 0; //Tentativas de comunicacao com o servidor
+		final int maxTry = 5; //Numero maximo de tentativas
 		
-		//Exibe o vetor
-		for(Integer x : vet)
-			System.out.print(x + " ");
+		do {
+			repeat = false;
+			tryCount++;
+			
+			try {
+				result = Comunicate(type, order, vet);
+				if(result == null) {
+					System.out.println("Servidor retornou nulo.");
+				}
+			} catch (RemoteException e) {
+				e.printStackTrace(System.out);
+				
+				System.out.println("Erro remoto");
+				
+				if(tryCount < maxTry) {
+					System.out.println("Tentanto novamente em 3s...");
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e1) {
+						System.out.println("Sleep interrompido");
+					}
+					
+					repeat = true;
+				}
+				
+			} catch (NotBoundException e) {
+				e.printStackTrace(System.out);
+				
+				System.out.println("Objeto remoto não encontrado.");
+				
+				if(tryCount < maxTry) {
+					System.out.println("Tentanto novamente em 3s...");
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e1) {
+						System.out.println("Sleep interrompido");
+					}
+					
+					repeat = true;
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} while(repeat);
 		
-		System.out.println();
-		System.out.println("Tempo gasto: " + String.format("%.5f", result.getValue() / 1e9) + "s" );
+		if(result != null) {
+			
+			if(result.getKey() == null) {
+				System.out.println("Vetor retornado é nulo");
+			} else {
+				vet = result.getKey();
+				
+				//Exibe o vetor
+				for(Integer x : vet)
+					System.out.print(x + " ");
+				System.out.println();
+
+				if(result.getValue() == null) {
+					System.out.println("Tempo retornado é nulo");
+				} else {
+					System.out.println("Tempo gasto no servidor: " + String.format("%.5f", result.getValue() / 1e9) + "s" );
+				}
+			}
+		}
 	}
 	
-	static public Pair<Integer[], Long> Comunicate(Compute.SortType type, Compute.Order order, Integer[] vet) throws MalformedURLException,
-		RemoteException, NotBoundException {
+	static public Pair<Integer[], Long> Comunicate(Compute.SortType type, Compute.Order order, Integer[] vet) 
+			throws MalformedURLException, RemoteException, NotBoundException {
 		
 		//Instancia o stub do cliente
 		Compute stub = (Compute) Naming.lookup("rmi://localhost:" + Compute.port + "/compute");
