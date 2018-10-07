6e7961c8350c9471073e427ef82b4b56ab232064
Victor <victor.agnez@hotmail.com>
2018-10-07 15:52:24 -0300

    try to send json file

--- SortAPI/src/client/client.cpp
+++ SortAPI/src/client/client.cpp
@@ -1,56 +1,99 @@
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
 
-using namespace utility;                    // Common utilities like string conversions
-using namespace web;                        // Common features like URIs.
-using namespace web::http;                  // Common HTTP functionality
-using namespace web::http::client;          // HTTP client features
-using namespace concurrency::streams;       // Asynchronous streams
+using namespace utility;					// Common utilities like string conversions
+using namespace web;						// Common features like URIs.
+using namespace web::http;				  // Common HTTP functionality
+using namespace web::http::client;		  // HTTP client features
+using namespace concurrency::streams;	   // Asynchronous streams
 
 int main(){
 	
 	auto fileStream = std::make_shared<ostream>();
 
-     // Open stream to output file.
-    pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile)
-    {
-        *fileStream = outFile;
+	 // Open stream to output file.
+	pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile)
+	{
+		*fileStream = outFile;
 
-        // Create http_client to send the request.
-        http_client client(U("http://localhost:8080/SortAPI/rest/hello"));
+		// Create http_client to send the request.
+		http_client client(U("http://localhost:8080/SortAPI/rest/hello"));
 
-        // Build request URI and start the request.
-        uri_builder builder(U("/sortArray"));
-        builder.append_query(U("sort"), U("Radix"));
-        builder.append_query(U("list"), U("2,1,-10,0"));
-        //std::cout << builder.to_string() << std::endl;
-        return client.request(methods::GET, builder.to_string());
-    })
+		// Build request URI and start the request.
+		
+		//Query pelo URL com retorno em texto:
+		/*
+		uri_builder builder(U("/sortArray"));
+		builder.append_query(U("sort"), U("Radix"));
+		builder.append_query(U("list"), U("2,1,-10,0"));
+		
+		//std::cout << builder.to_string() << std::endl;
+		return client.request(methods::GET, builder.to_string()); */
+		
+		//Query pelo URL com retorno em JSON:
+		/*
+		uri_builder builder(U("/sortArrayJSON"));
+		builder.append_query(U("sort"), U("Radix"));
+		builder.append_query(U("list"), U("2,1,-10,0"));
+		
+		//std::cout << builder.to_string() << std::endl;
+		return client.request(methods::GET, builder.to_string()); */
+		
+		//Query pelo JSON:
+		
+		try {
+			uri_builder builder(U("/sortArrayJSONPOST"));
+			std::string textFile = "";
+			string_t fileName("test.json");
+		
+			ifstream_t	  f(fileName);							  // filestream of working file
+			stringstream_t  s;										  // string stream for holding JSON read from file
+			json::value	 v;										  // JSON read from input file
+			
+			if (f) {													
+				s << f.rdbuf();										 // stream results of reading from file stream into string stream
+				f.close();											  // close the filestream
 
-    // Handle response headers arriving.
-    .then([=](http_response response)
-    {
-        printf("Received response status code:%u\n", response.status_code());
+				v.parse(s);											 // parse the resultant string stream.
+			}
+			
+			//return client.request(methods::POST, builder.to_string(), v); //tentativa 1
+			const std::string tmp = s.str();
+			
+			return client.request(methods::POST, builder.to_string(), 
+				utility::conversions::to_utf8string(tmp), _XPLATSTR("application/json")); //tentativa 2
+		}
+		catch (web::json::json_exception excep) {
+			std::cout << "ERROR Parsing JSON: ";
+			std::cout << excep.what();
+		}
+		
+	})
 
-        // Write response body into the file.
-        return response.body().read_to_end(fileStream->streambuf());
-    })
+	// Handle response headers arriving.
+	.then([=](http_response response)
+	{
+		printf("Received response status code:%u\n", response.status_code());
 
-    // Close the file stream.
-    .then([=](size_t)
-    {
-        return fileStream->close();
-    });
+		// Write response body into the file.
+		return response.body().read_to_end(fileStream->streambuf());
+	})
 
-    // Wait for all the outstanding I/O to complete and handle any exceptions
-    try
-    {
-        requestTask.wait();
-    }
-    catch (const std::exception &e)
-    {
-        printf("Error exception:%s\n", e.what());
-    }
+	// Close the file stream.
+	.then([=](size_t)
+	{
+		return fileStream->close();
+	});
 
-    return 0;
+	// Wait for all the outstanding I/O to complete and handle any exceptions
+	try
+	{
+		requestTask.wait();
+	}
+	catch (const std::exception &e)
+	{
+		printf("Error exception:%s\n", e.what());
+	}
+
+	return 0;
 }
